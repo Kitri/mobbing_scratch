84f506c1839a04cdc84a942be81c152ff0a71561
Mercia Malan <mercia.malan@thameswater.co.uk>
2023-01-20 12:56:46 +0200

    Refactor up to 5

--- test_tdd.py
+++ test_tdd.py
@@ -23,17 +23,38 @@ XI, XII, XIII, XIV, XV, XVI, XVII, XVIII, XIX, XX
 """
 
 def convert_roman_numeral(roman_numeral: str) -> int:
-    if 'V' in roman_numeral:
-        return 4
-    else:
+    roman_table: dict = {
+        'I': 1,
+        'V': 5
+    }
+
+    if len(roman_numeral) == 1:
+        return roman_table[roman_numeral]
+    elif 'V' not in roman_numeral:
         return len(roman_numeral)
+    else:
+        last_char: int = roman_table[roman_numeral[-1]] 
+        first_char: int = roman_table[roman_numeral[0]] 
+
+        return last_char - first_char
+        
+
+    # II
+    # last_char = I = 1
+    # I (1) < 1
+    # III
+    # last_char = 1
+    # I -> 1; I -> 1
+
+
 
 
 testdata = [
     ('I',1),
     ('II',2),
     ('III',3),
-    ('IV',4)
+    ('IV',4),
+    ('V',5),
 ]
 @pytest.mark.parametrize("roman_numeral, expected_int", testdata)
 def test_roman_numeral_conversion(roman_numeral, expected_int):

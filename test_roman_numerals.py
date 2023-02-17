e3f123fc8feaf48b6f6e79d48494285aec9872bd
Mercia Malan <mercia.malan@thameswater.co.uk>
2023-02-17 13:01:17 +0200

    Complete up to 10

--- test_roman_numerals.py
+++ test_roman_numerals.py
@@ -21,18 +21,8 @@ X - 10
 XI, XII, XIII, XIV, XV, XVI, XVII, XVIII, XIX, XX
 
 """
-
-def convert_roman_numeral(roman_numeral: str) -> int:
-    roman_table: dict = {
-        'I': 1,
-        'V': 5
-    }
-
-    if len(roman_numeral) == 1:
-        return roman_table[roman_numeral]
-    elif 'V' not in roman_numeral:
-        return len(roman_numeral)
-    else:
+# TEST HERE -->>
+def evaluate_length_2(roman_numeral: str, roman_table):
         last_char: int = roman_table[roman_numeral[-1]] 
         first_char: int = roman_table[roman_numeral[0]] 
 
@@ -40,8 +30,32 @@ def convert_roman_numeral(roman_numeral: str) -> int:
             return last_char - first_char
         else:
             return last_char + first_char
-        
 
+def convert_multiple_letters(roman_numeral:str, roman_table: dict):
+
+    running_decimal_val = 0
+
+    if roman_numeral.startswith("V"): 
+        running_decimal_val = roman_table["V"]
+        roman_numeral = roman_numeral[1:]
+
+    if ('V' not in roman_numeral) and ('X' not in roman_numeral):
+        return len(roman_numeral) + running_decimal_val
+    elif len(roman_numeral) == 2:
+        return evaluate_length_2(roman_numeral, roman_table)
+
+def convert_roman_numeral(roman_numeral: str) -> int:
+    roman_table: dict = {
+        'I': 1,
+        'V': 5, 
+        'X': 10
+    }
+
+    if len(roman_numeral) == 1:
+        return roman_table[roman_numeral]
+    else:
+        return convert_multiple_letters(roman_numeral, roman_table)
+      
     # II
     # last_char = I = 1
     # I (1) < 1
@@ -50,15 +64,17 @@ def convert_roman_numeral(roman_numeral: str) -> int:
     # I -> 1; I -> 1
 
 
-
-
 testdata = [
     ('I',1),
     ('II',2),
     ('III',3),
     ('IV',4),
     ('V',5),
-    ('VI',6)
+    ('VI',6),
+    ('VII',7),
+    ("VIII", 8), 
+    ("IX", 9),
+    ("X", 10),
 ]
 @pytest.mark.parametrize("roman_numeral, expected_int", testdata)
 def test_roman_numeral_conversion(roman_numeral, expected_int):
